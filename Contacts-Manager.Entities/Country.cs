ced6e10560923d8bd9ba0adb906f40eeadc49d32
Imran Saleh <126408093+imransaleh21@users.noreply.github.com>
2025-07-06 01:05:48 +0600

    Create Project Architucture(Clean arc.), Create DTO file, Make a Country entity model

---
+++ Contacts-Manager.Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities
+{
+    public class Country
+    {
+        /// <summary>
+        /// Domain Model for Country
+        /// </summary>
+        public Guid CountryID { get; set; }
+        public string? CountryName { get; set; }
+
+    }
+}
