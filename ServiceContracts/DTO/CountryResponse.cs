97ddfe1d03b70d04d18eb01301373bace3a567ca
Imran Saleh <126408093+imransaleh21@users.noreply.github.com>
2025-07-07 20:37:24 +0600

    A CountryResponse DTO is created and inside it a Extension function for convert the Country obj to DTO is placed. Also inside the DTO folder CountryRequest DTO is there

---
+++ ServiceContracts/DTO/CountryResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Entities;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO class to return response from most of the CountryServices
+    /// </summary>
+    public class CountryResponse
+    {
+        public Guid CountryID { get; set; }
+        public string? CountryName { get; set; }
+    }
+
+    public static class CountryExtensions 
+    {
+        // extension method of for Country class
+        public static CountryResponse ToCountryResponse(this Country country)
+        {
+            return new CountryResponse
+            {
+                CountryID = country.CountryID,
+                CountryName = country.CountryName
+            };
+        }
+    }
+
+}
