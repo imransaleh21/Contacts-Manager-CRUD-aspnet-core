8cc93b2bfb753eb478c884b7b1634cceea7dd5bc
Imran Saleh <126408093+imransaleh21@users.noreply.github.com>
2025-07-12 00:56:01 +0600

    override Equals method in CountryResponse to compare two objects value are same or not of the CountryResponse return trype class. In Unit test when we will use Assert.Contains() it will use this override method internally

--- ServiceContracts/DTO/CountryResponse.cs
+++ ServiceContracts/DTO/CountryResponse.cs
@@ -6,11 +6,43 @@ namespace ServiceContracts.DTO
     /// <summary>
     /// DTO class for representing a country response of most of the Country Service methods
     /// </summary>
-    public class CountryResponse
+    public class CountryResponse : IEquatable<CountryResponse>
     {
         public Guid CountryID { get; set; }
         public string? CountryName { get; set; }
+
+        // The Equals method is an implementation of the IEquatable<CountryResponse> interface method. 
+        // It is used to determine whether the current CountryResponse object is equal to another CountryResponse object 
+        // by comparing their CountryID and CountryName properties.
+        public bool Equals(CountryResponse? countryToCompare)
+        {
+            if (countryToCompare is null)
+                return false;
+
+            return CountryID == countryToCompare.CountryID
+                   && CountryName == countryToCompare.CountryName;
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current CountryResponse instance.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current CountryResponse.</param>
+        /// <returns>true if the specified object is a CountryResponse and has the same CountryID and CountryName; otherwise, false.</returns>
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as CountryResponse); // Use the Equals method defined above
+        }
+
+        /// <summary>
+        /// Returns a hash code for the current CountryResponse instance.
+        /// </summary>
+        /// <returns>A hash code based on the CountryID and CountryName properties.</returns>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(CountryID, CountryName);
+        }
     }
+
 
     public static class CountryExtensions 
     {
