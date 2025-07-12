370af0f92d582944956b4706cc8c25b5f1893f3c
Imran Saleh <126408093+imransaleh21@users.noreply.github.com>
2025-07-13 01:50:55 +0600

    Crate Person Model Class to store Person, Create a DTO to add a person into the list and for Gender option make a Enums

---
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Entities;
+using ServiceContracts.Enums;
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// This DTO is used for inserting a new person
+    /// </summary>
+    internal class PersonAddRequest
+    {
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public GenderOptions? Gender { get; set; }
+        public Guid? CountryId { get; set; }
+        public string? Address { get; set; }
+        public bool ReceiveNewsLettter { get; set; }
+
+        //Convert the PersonAddRequest Object to Person Object
+        public Person ToPerson()
+        {
+            return new()
+            {
+                PersonName = PersonName,
+                Email = Email,
+                DateOfBirth = DateOfBirth,
+                Gender = Gender.ToString(),
+                CountryId = CountryId,
+                Address = Address,
+                ReceiveNewsLettter = ReceiveNewsLettter,
+            };
+        }
+    }
+}
