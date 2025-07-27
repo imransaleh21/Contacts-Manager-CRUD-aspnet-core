11891da58b567c214a9f2d711cb4fbd10e302022
Imran Saleh <126408093+imransaleh21@users.noreply.github.com>
2025-07-27 23:57:35 +0600

    Create the controller and make the index view, So, the full structure is now ready

---
+++ Contacts-Manager-CRUD/Controllers/PersonsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contacts_Manager_CRUD.Controllers
+{
+    public class PersonsController : Controller
+    {
+        [Route("persons/index")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
