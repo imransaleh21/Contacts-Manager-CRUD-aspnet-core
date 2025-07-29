b7983dfc9d538304473c521fd41202d0ee3ce808
Imran Saleh <126408093+imransaleh21@users.noreply.github.com>
2025-07-29 23:07:04 +0600

    Add the search field in the index view and fetch searchBy options from controller. After that send the values of searchBy and searchValue via get api from the index view, also get this value in the index method's parameter

--- Contacts-Manager-CRUD/Controllers/PersonsController.cs
+++ Contacts-Manager-CRUD/Controllers/PersonsController.cs
@@ -17,8 +17,17 @@ namespace Contacts_Manager_CRUD.Controllers
         /// <returns></returns>
         [Route("persons/index")]
         [Route("/")]
-        public IActionResult Index()
+        public IActionResult Index(string searchBy, string searchValue)
         {
+            ViewBag.SearchField = new Dictionary<string, string>
+            {
+                { nameof(PersonResponse.PersonName),  "Person Name" },
+                { nameof(PersonResponse.Email), "Email"},
+                { nameof(PersonResponse.DateOfBirth), "Date of Birth" },
+                { nameof(PersonResponse.Gender), "Gender"},
+                { nameof(PersonResponse.CountryId), "Country"},
+                { nameof(PersonResponse.Address), "Address"}
+            };
             // Retrieve all persons from the service
             List<PersonResponse> persons = _personsService.GetAllPersons();
             return View(persons); // Return the view with the list of persons at Views/Persons/Index.cshtml
