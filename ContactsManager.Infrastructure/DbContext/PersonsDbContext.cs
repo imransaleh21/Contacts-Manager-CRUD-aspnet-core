30b6a5cc9476b559e800412e97cd94ff8e7004cd
Imran Saleh <126408093+imransaleh21@users.noreply.github.com>
2026-01-11 02:04:28 +0600

    Add infrastructure layer to the architecture and past DbContext and repositories inside it

---
+++ ContactsManager.Infrastructure/DbContext/PersonsDbContext.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+namespace Entities
+{
+    public class PersonsDbContext : DbContext
+    {
+        public PersonsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public virtual DbSet<Country> Countries { get; set; }
+        public virtual DbSet<Person> Persons { get; set; }
+
+        /// <summary>
+        /// OnModelCreating is overridden from DbContext class and is used to configure the model
+        /// like setting table names, relationships, primary keys, relationships, constraints, seeding data etc.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // Call the base method to ensure any base configurations are applied
+            // Configure the table names for the entities
+            modelBuilder.Entity<Country>().ToTable("Countries");
+            modelBuilder.Entity<Person>().ToTable("Persons", t =>
+            {
+                // Fluent Api to add check constraint on PIN column of Persons table
+                t.HasCheckConstraint("CHK_PIN", "len([PIN]) = 4");
+            });
+
+            // Seed initial data for Countries
+
+            // we can seed data one by one like this
+            //modelBuilder.Entity<Country>().HasData(
+            //    new Country { CountryID = Guid.NewGuid(), CountryName = "United States" },
+            //);
+
+            // Or, also we can use a collection(like json array) to seed multiple records at once
+            // like below uncommented code
+            string countriesJson = System.IO.File.ReadAllText("countries.json");
+           List<Country>? countries = System.Text.Json.JsonSerializer.Deserialize<List<Country>>(countriesJson);
+            foreach(Country country in countries)
+            {
+                modelBuilder.Entity<Country>().HasData(country);
+            }
+
+            // Seed initial data for Persons
+            string personsJson = System.IO.File.ReadAllText("persons.json");
+            List<Person>? persons = System.Text.Json.JsonSerializer.Deserialize<List<Person>>(personsJson);
+            foreach(Person person in persons)
+            {
+                //person.PersonId = Guid.NewGuid(); // Ensure each person has a unique ID
+                modelBuilder.Entity<Person>().HasData(person);
+            }
+
+            // Fluent Api example
+            // Use Fluent Api to Configure the PIN property of Person entity to have a specific column type
+            modelBuilder.Entity<Person>().Property(column => column.PIN)
+                .HasColumnType("varchar(6)");
+
+            //modelBuilder.Entity<Person>().HasIndex(c => c.PIN).IsUnique(); // to set unique column
+        }
+
+        /// <summary>
+        /// Stored procedure to get all persons
+        /// </summary>
+        /// <returns></returns>
+        public List<Person> sp_GetAllPersons()
+        {
+            return Persons.FromSqlRaw("EXECUTE [dbo].[GetAllPersons]").ToList();
+        }
+
+        public async Task<int> sp_InsertPerson(Person person)
+        {
+            SqlParameter[] sqlParameters = new SqlParameter[]
+            {
+                new SqlParameter("@PersonId", person.PersonId),
+                new SqlParameter("@PersonName", person.PersonName ?? (object)DBNull.Value),
+                new SqlParameter("@Email", person.Email ?? (object)DBNull.Value),
+                new SqlParameter("@DateOfBirth", person.DateOfBirth ?? (object)DBNull.Value),
+                new SqlParameter("@Gender ", person.Gender ?? (object)DBNull.Value),
+                new SqlParameter("@CountryId", person.CountryId ?? (object)DBNull.Value),
+                new SqlParameter("@Address", person.Address ?? (object)DBNull.Value),
+                new SqlParameter("@ReceiveNewsLettter", person.ReceiveNewsLettter ?? (object)DBNull.Value)
+            };
+
+            return await Database.ExecuteSqlRawAsync("EXECUTE [dbo].[InsertPerson] " +
+                "@PersonId, @PersonName, @Email, @DateOfBirth, @Gender, @CountryId," +
+                " @Address, @ReceiveNewsLettter", sqlParameters);
+        }
+    }
+}
---
+++ ContactsManager.Infrastructure/Repositories/PersonsRepository.cs
@@ -0,0 +1,74 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class PersonsRepository : IPersonsRepository
+    {
+        private readonly PersonsDbContext _db;
+        public PersonsRepository(PersonsDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<Person> AddPerson(Person person)
+        {
+            _db.Persons.Add(person);
+            await _db.SaveChangesAsync();
+            return person;
+        }
+
+        public async Task<bool> DeletePersonByPersonID(Guid personId)
+        {
+            _db.Persons.Remove(_db.Persons.FirstOrDefault(p => p.PersonId == personId)!);
+            return await _db.SaveChangesAsync().ContinueWith(task => task.Result > 0);
+        }
+
+        public async Task<List<Person>> GetAllPersons()
+        {
+            return await _db.Persons.Include("Country").ToListAsync();
+        }
+
+        public async Task<List<Person>> GetFilteredPersons(Expression<Func<Person, bool>> predicate)
+        {
+            return await _db.Persons.Include("Country")
+                .Where(predicate)
+                .ToListAsync();
+        }
+
+        public async Task<Person?> GetPersonByPersonId(Guid? personId)
+        {
+            return await _db.Persons.Include("Country")
+                .FirstOrDefaultAsync(p => p.PersonId == personId);
+        }
+
+        public async Task<Person> UpdatePerson(Person person)
+        {
+            Person? matchedPerson = await GetPersonByPersonId(person.PersonId);
+            _db.Persons.Update(person);
+            /*
+             * As person info is found(not null), So, person info of that person id is found,
+             * now we can update the Person info of that person
+             * here as matchedPerson is already being tracked by EF Core,
+             * so no need to call _db.Persons.Update(matchedPerson) method
+            */
+            matchedPerson.PersonName = person.PersonName;
+            matchedPerson.Email = person.Email;
+            matchedPerson.DateOfBirth = person.DateOfBirth;
+            matchedPerson.Gender = person.Gender;
+            matchedPerson.CountryId = person.CountryId;
+            matchedPerson.Address = person.Address;
+            matchedPerson.ReceiveNewsLettter = person.ReceiveNewsLettter;
+            matchedPerson.PIN = person.PIN;
+
+            await _db.SaveChangesAsync();
+            return matchedPerson;
+        }
+    }
+}
