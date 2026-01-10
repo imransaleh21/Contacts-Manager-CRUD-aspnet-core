14ac9919b5d0b4dfd0d0a5df0840bea5f4acbd67
Imran Saleh <126408093+imransaleh21@users.noreply.github.com>
2026-01-11 04:08:53 +0600

    Move the unit test files as per the clean architecture policies into different class library

---
+++ ContactsManager.IntegrationTests/PersonsControllerIntegrationTest.cs
@@ -0,0 +1,34 @@
+﻿using Fizzler;
+using Fizzler.Systems.HtmlAgilityPack;
+using FluentAssertions;
+using HtmlAgilityPack;
+namespace xUnitTests
+{
+    public class PersonsControllerIntegrationTest : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+        public PersonsControllerIntegrationTest(CustomWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+        #region Index Tests
+        [Fact]
+        public async Task Index_ReturnsViewResult()
+        {
+            //Arrange
+            //Act
+            HttpResponseMessage httpResponseMessage = await _client.GetAsync("/Persons/Index");
+            //Assert
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            // Below code is to verify whether the response body contains the table with class personsList for intregration testing
+            string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+            HtmlDocument htmlDocument = new();
+            htmlDocument.LoadHtml(responseBody);
+
+            var document = htmlDocument.DocumentNode;
+            document.QuerySelectorAll("table.personsList").Should().NotBeNull();
+        }
+        #endregion
+    }
+}
